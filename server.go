e24dd9aa6adb50666299efcabd38d16edfcf1df5
Henrik Hautakoski <henrik@eossweden.org>
2022-01-04 09:15:25 +0100

    Convert hard tabs to soft

--- server.go
+++ server.go
@@ -2,55 +2,55 @@
 package tcp_server
 
 import (
-	"net"
+    "net"
 )
 
 func New(address string) *server {
 
-	server := &server{
-		address: address,
-	}
+    server := &server{
+        address: address,
+    }
 
-	server.OnConnect(func(c *Client) {})
-	server.OnMessage(func(c *Client, message string) {})
-	server.OnDisconnect(func(c *Client, err error) {})
+    server.OnConnect(func(c *Client) {})
+    server.OnMessage(func(c *Client, message string) {})
+    server.OnDisconnect(func(c *Client, err error) {})
 
-	return server
+    return server
 }
 
 // Called when a client connects
 func (s *server) OnConnect(callback func(c *Client)) {
-	s.onConnect = callback
+    s.onConnect = callback
 }
 
 // Called the server gets a message from a client.
 func (s *server) OnMessage(callback func(c *Client, message string)) {
-	s.onMessage = callback
+    s.onMessage = callback
 }
 
 // Called when a connection is closed.
 func (s *server) OnDisconnect(callback func(c *Client, err error)) {
-	s.onDisconnect = callback
+    s.onDisconnect = callback
 }
 
 func (s *server) Listen() error {
 
-	sock, err := net.Listen("tcp", s.address)
-	if err != nil {
-		return err
-	}
-	defer sock.Close()
+    sock, err := net.Listen("tcp", s.address)
+    if err != nil {
+        return err
+    }
+    defer sock.Close()
 
-	for {
-		conn, _ := sock.Accept()
-		c := &Client{
-			conn:   conn,
-			Server: s,
-			Addr: conn.RemoteAddr(),
-		}
-		s.onConnect(c)
-		go c.read()
-	}
+    for {
+        conn, _ := sock.Accept()
+        c := &Client{
+            conn:   conn,
+            Server: s,
+            Addr:   conn.RemoteAddr(),
+        }
+        s.onConnect(c)
+        go c.read()
+    }
 
-	return nil
+    return nil
 }
--- types.go
+++ types.go
@@ -2,18 +2,18 @@
 package tcp_server
 
 import (
-	"net"
+    "net"
 )
 
 type Client struct {
-	conn   net.Conn
-	Addr   net.Addr
-	Server *server
+    conn    net.Conn
+    Addr    net.Addr
+    Server  *server
 }
 
 type server struct {
-	address 		string
-	onConnect		func(c *Client)
-	onDisconnect	func(c *Client, err error)
-	onMessage		func(c *Client, message string)
+    address         string
+    onConnect       func(c *Client)
+    onDisconnect    func(c *Client, err error)
+    onMessage       func(c *Client, message string)
 }
