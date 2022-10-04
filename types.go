d15a74d24d7c69684f35ab9993329c576ce4579c
Henrik Hautakoski <henrik@eossweden.org>
2022-10-04 10:38:21 +0200

    types.go: Adding some comments.

--- types.go
+++ types.go
@@ -5,15 +5,28 @@ import (
     "net"
 )
 
+// Struct that represents a client connection
 type Client struct {
+
+    // Actual network connections.
     conn    net.Conn
+
+    // Client address
     Addr    net.Addr
+
+    // Pointer to server object.
     Server  *server
 }
 
+// Struct that represents a network listener
 type server struct {
+    // Actual listener connection
     listener        net.Listener
+
+    // Address to bind the server on
     address         string
+
+    // Callback functions
     onConnect       func(c *Client)
     onDisconnect    func(c *Client, err error)
     onMessage       func(c *Client, message string)
