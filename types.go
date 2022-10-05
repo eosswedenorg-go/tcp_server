bd259e1fd44340f85d1fd7fff5318d681d78f545
Henrik Hautakoski <henrik@eossweden.org>
2022-10-05 12:44:30 +0200

    types.go: Adding server.running

--- types.go
+++ types.go
@@ -30,6 +30,11 @@ type server struct {
     // WaitGroup to make all go routines exit gracefully.
     wg              sync.WaitGroup
 
+    // Simple bool to signal that we are in listener loop.
+    // Clients should never write to this (only server.Close() may do so)
+    // but read the value and exit when it's false.
+    running         bool
+
     // Callback functions
     onConnect       func(c *Client)
     onDisconnect    func(c *Client, err error)
